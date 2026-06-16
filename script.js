c5f6909185ea749bba45aea221ffdb2627745009
mani-538 <manikandan.aids.25@gmail.com>
2026-06-16 19:30:51 +0530

    Save tasks to local storage and Load tasks from local storage

--- script.js
+++ script.js
@@ -2,18 +2,37 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 
-addTaskBtn.addEventListener("click", function () {
+const allBtn = document.getElementById("allBtn");
+const activeBtn = document.getElementById("activeBtn");
+const completedBtn = document.getElementById("completedBtn");
 
-    const taskText = taskInput.value.trim();
+function saveTasks() {
 
-    if (taskText === "") {
-        return;
+    const tasks = [];
+
+    const taskItems = taskList.children;
+
+    for (let task of taskItems) {
+
+        tasks.push({
+            text: task.childNodes[0].textContent.trim(),
+            completed: task.dataset.completed === "true"
+        });
+
     }
 
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function createTask(taskText, completed = false) {
     const li = document.createElement("li");
 
-    const checkbox = document.createElement("input");
+    li.dataset.completed = "false";
+
+const checkbox = document.createElement("input");
 checkbox.type = "checkbox";
+
+checkbox.checked = completed;
 
 const deleteBtn = document.createElement("button");
 
@@ -27,17 +46,98 @@ checkbox.addEventListener("change", function () {
         li.style.textDecoration = "none";
     }
 
+    li.dataset.completed = completed;
+
+    if (completed) {
+    li.style.textDecoration = "line-through";
+}
+
+    saveTasks();
+
 });
 
 deleteBtn.textContent = "Delete";
 
 deleteBtn.addEventListener("click", function () {
+
     li.remove();
+
+    saveTasks();
+
 });
 
+li.appendChild(checkbox);
 li.appendChild(deleteBtn);
 
 taskList.appendChild(li);
 
+}
+
+addTaskBtn.addEventListener("click", function () {
+
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        return;
+    }
+
+    createTask(taskText);
+
+saveTasks();
+
     taskInput.value = "";
 });
+
+allBtn.addEventListener("click", function () {
+
+    const tasks = taskList.children;
+
+    for (let task of tasks) {
+        task.style.display = "block";
+    }
+
+});
+
+activeBtn.addEventListener("click", function () {
+
+    const tasks = taskList.children;
+
+    for (let task of tasks) {
+
+        if (task.dataset.completed === "true") {
+            task.style.display = "none";
+        } else {
+            task.style.display = "block";
+        }
+
+    }
+
+});
+
+completedBtn.addEventListener("click", function () {
+
+    const tasks = taskList.children;
+
+    for (let task of tasks) {
+
+        if (task.dataset.completed === "true") {
+            task.style.display = "block";
+        } else {
+            task.style.display = "none";
+        }
+
+    }
+
+});
+
+const savedTasks =
+    JSON.parse(localStorage.getItem("tasks")) || [];
+
+for (let task of savedTasks) {
+
+    createTask(
+        task.text,
+        task.completed
+    );
+
+}
